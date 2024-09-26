ca5e9e300aeac92691cee214bb26d6bda19eec3e
Tharakesh Pulikonda <tarakesh.p@gmail.com>
2024-09-26 18:53:54 +0530

    Create prototype-inheritance.js

---
+++ src/prototype-inheritance.js
@@ -0,0 +1,20 @@
+function Vehicle(p1) {
+    this._p1 = p1;
+}
+
+Vehicle.prototype.drive = function() {
+     console.log('driving ' + this._p1);
+  }
+
+function Car(p1) {
+   Vehicle.call(this, p1); 
+}
+
+// Car.prototype.constructor = Vehicle;
+// Car.prototype = new Vehicle();
+
+Car.prototype = Object.create( Vehicle.prototype );
+
+
+var car = new Car('test');
+car.drive(); // driving test
